ebe9becc34d0671303667ed9ebc21e75eb2259eb
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-04-24 15:23:52 -0600

    feat: cars detects traffic light

--- app/src/main/java/mx/tecnm/tepic/ladm_u2_practica1_semaforo/Hilo.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u2_practica1_semaforo/Hilo.kt
@@ -2,6 +2,8 @@ package mx.tecnm.tepic.ladm_u2_practica1_semaforo
 
 import android.util.Log
 
+var semaforo=true
+
 class Hilo(c:Lienzo,carro:Int):Thread(){
     val canvas:Lienzo = c
     val car= carro
@@ -10,48 +12,44 @@ class Hilo(c:Lienzo,carro:Int):Thread(){
         when(car){
             1 -> {
                 while (true){
-                    if (posCar1>-600F) posCar1-=1F
-                    if (posCar1<=-600F) {
+                    if (!(semaforo && posCar1 == 1270F)) posCar1 -= 1F
+                    if (posCar1 <= -600F) {
                         posCar1 = 2600F
-                        temp=(1..15).random()
+                        temp = (1..15).random()
                     }
-                    Log.d("Posx", posCar1.toString()+"-Temp: "+temp)
                     canvas.invalidate()
                     sleep(temp.toLong())
                 }
             }
             2 -> {
                 while (true){
-                    if (posCar2<1080F) posCar2+=1F
-                    if (posCar2>=1080F) {
+                    if (!(!semaforo && posCar2==-140F))posCar2 += 1F
+                    if (posCar2 >= 1080F) {
                         posCar2 = -600F
-                        temp=(1..15).random()
+                        temp = (1..15).random()
                     }
-                    Log.d("Posx", posCar2.toString()+"-Temp: "+temp)
                     canvas.invalidate()
                     sleep(temp.toLong())
                 }
             }
             3 -> {
                 while (true){
-                    if (posCar3<2500F) posCar3+=1F
-                    if (posCar3>=2500F) {
+                    if (!(semaforo && posCar3 == 320F)) posCar3 += 1F
+                    if (posCar3 >= 2500F) {
                         posCar3 = -600F
-                        temp=(1..15).random()
+                        temp = (1..15).random()
                     }
-                    Log.d("Posx", posCar3.toString()+"-Temp: "+temp)
                     canvas.invalidate()
                     sleep(temp.toLong())
                 }
             }
             4 -> {
                 while (true){
-                    if (posCar4>-600F) posCar4-=1F
-                    if (posCar4<=-600F) {
+                    if (!(!semaforo && posCar4==810F)) posCar4 -= 1F
+                    if (posCar4 <= -600F) {
                         posCar4 = 1280F
-                        temp=(1..15).random()
+                        temp = (1..15).random()
                     }
-                    Log.d("Posx", posCar4.toString()+"-Temp: "+temp)
                     canvas.invalidate()
                     sleep(temp.toLong())
                 }
@@ -82,8 +80,9 @@ class Hilo(c:Lienzo,carro:Int):Thread(){
                     semInf[0] = "#9D281A"
                     semInf[1] = "#B0A121"
                     semInf[2] = "#6DEF14"
+                    semaforo=false
                     canvas.invalidate()
-                    sleep(2000)
+                    sleep(5000)
                     //Amarillo
                     semSup[0] = "#9D281A"
                     semSup[1] = "#FDE730"
@@ -102,8 +101,9 @@ class Hilo(c:Lienzo,carro:Int):Thread(){
                     semInf[0] = "#EA3C28"
                     semInf[1] = "#B0A121"
                     semInf[2] = "#4AA20D"
+                    semaforo=true
                     canvas.invalidate()
-                    sleep(2000)
+                    sleep(5000)
                 }
             }
         }
