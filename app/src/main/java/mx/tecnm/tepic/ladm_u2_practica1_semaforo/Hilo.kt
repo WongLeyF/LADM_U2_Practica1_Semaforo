fb23b8203e7a24d3e7bd4daf2cd1472f577c8f94
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-04-25 17:12:12 -0600

    feat: cars detects others cars

--- app/src/main/java/mx/tecnm/tepic/ladm_u2_practica1_semaforo/Hilo.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u2_practica1_semaforo/Hilo.kt
@@ -7,15 +7,20 @@ var semaforo=true
 class Hilo(c:Lienzo,carro:Int):Thread(){
     val canvas:Lienzo = c
     val car= carro
-    var temp=(1..15).random()
+    var temp=(10..75).random()
     override fun run() {
+
         when(car){
             1 -> {
                 while (true){
-                    if (!(semaforo && posCar1 == 1270F)) posCar1 -= 1F
+                    if (!(semaforo && posCar1 == 1270F)){
+                        if (!(getWait24()&&posCar1 == 1270F)) {
+                            posCar1 -= 5F
+                        }
+                    }
                     if (posCar1 <= -600F) {
                         posCar1 = 2600F
-                        temp = (1..15).random()
+                        temp = (10..75).random()
                     }
                     canvas.invalidate()
                     sleep(temp.toLong())
@@ -23,10 +28,15 @@ class Hilo(c:Lienzo,carro:Int):Thread(){
             }
             2 -> {
                 while (true){
-                    if (!(!semaforo && posCar2==-140F))posCar2 += 1F
+                    if (!(!semaforo && posCar2==-140F)){
+                        if (!(getWait13()&&posCar2==-140F)){
+                            posCar2 +=5F
+                        }
+                    }
                     if (posCar2 >= 1080F) {
                         posCar2 = -600F
-                        temp = (1..15).random()
+                        temp = (10..75).random()
+
                     }
                     canvas.invalidate()
                     sleep(temp.toLong())
@@ -34,10 +44,14 @@ class Hilo(c:Lienzo,carro:Int):Thread(){
             }
             3 -> {
                 while (true){
-                    if (!(semaforo && posCar3 == 320F)) posCar3 += 1F
+                    if (!(semaforo && posCar3 == 320F)){
+                        if (!(getWait24()&&posCar3 == 320F)) {
+                            posCar3 +=5F
+                        }
+                    }
                     if (posCar3 >= 2500F) {
                         posCar3 = -600F
-                        temp = (1..15).random()
+                        temp = (10..75).random()
                     }
                     canvas.invalidate()
                     sleep(temp.toLong())
@@ -45,68 +59,25 @@ class Hilo(c:Lienzo,carro:Int):Thread(){
             }
             4 -> {
                 while (true){
-                    if (!(!semaforo && posCar4==810F)) posCar4 -= 1F
+                    if (!(!semaforo && posCar4==810F)) {
+                        if (!(getWait13() && posCar4==810F)){
+                            posCar4 -= 5F
+                        }
+                    }
                     if (posCar4 <= -600F) {
                         posCar4 = 1280F
-                        temp = (1..15).random()
+                        temp = (10..75).random()
                     }
                     canvas.invalidate()
                     sleep(temp.toLong())
                 }
             }
-            5-> {
-                //rojo encendido = #EA3C28 Ap= #9D281A
-                //verde encendido = #6DEF14 Ap= #4AA20D
-                //amarillo enc = #FDE730 Ap = #B0A121
-                while (true){
-                    //Amarillo
-                    semSup[0] = "#9D281A"
-                    semSup[1] = "#FDE730"
-                    semSup[2] = "#4AA20D"
-
-                    semInf[0] = "#9D281A"
-                    semInf[1] = "#FDE730"
-                    semInf[2] = "#4AA20D"
-                    canvas.invalidate()
-                    sleep(2000)
-                    //Verde inferior
-                    //rojo encendido = #EA3C28 Ap= #9D281A
-                    //verde encendido = #6DEF14 Ap= #4AA20D
-                    //amarillo enc = #FDE730 Ap = #B0A121
-                    semSup[0] = "#EA3C28"
-                    semSup[1] = "#B0A121"
-                    semSup[2] = "#4AA20D"
-
-                    semInf[0] = "#9D281A"
-                    semInf[1] = "#B0A121"
-                    semInf[2] = "#6DEF14"
-                    semaforo=false
-                    canvas.invalidate()
-                    sleep(5000)
-                    //Amarillo
-                    semSup[0] = "#9D281A"
-                    semSup[1] = "#FDE730"
-                    semSup[2] = "#4AA20D"
-
-                    semInf[0] = "#9D281A"
-                    semInf[1] = "#FDE730"
-                    semInf[2] = "#4AA20D"
-                    canvas.invalidate()
-                    sleep(2000)
-                    //Verde Superior
-                    semSup[0] = "#9D281A"
-                    semSup[1] = "#B0A121"
-                    semSup[2] = "#6DEF14"
-
-                    semInf[0] = "#EA3C28"
-                    semInf[1] = "#B0A121"
-                    semInf[2] = "#4AA20D"
-                    semaforo=true
-                    canvas.invalidate()
-                    sleep(5000)
-                }
-            }
         }
-
+    }
+    private fun getWait13():Boolean{
+        return (posCar1<1260F&& posCar1>350F)||(posCar3<1260F&& posCar3>350F)
+    }
+    private fun getWait24():Boolean{
+        return (posCar2<810F && posCar2>-140F)||(posCar4<810F&& posCar4>-140F)
     }
 }
