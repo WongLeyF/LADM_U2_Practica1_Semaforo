447e0661dbe61352c6e6e00df70a302081807e85
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-04-25 17:15:53 -0600

    feat: refactoring semaforo

--- app/src/main/java/mx/tecnm/tepic/ladm_u2_practica1_semaforo/MainActivity.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u2_practica1_semaforo/MainActivity.kt
@@ -2,6 +2,8 @@ package mx.tecnm.tepic.ladm_u2_practica1_semaforo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +14,12 @@ class MainActivity : AppCompatActivity() {
         val hilo2 = Hilo(lienzo.getLienzo(),2)
         val hilo3 = Hilo(lienzo.getLienzo(),3)
         val hilo4 = Hilo(lienzo.getLienzo(),4)
-        val hilo5 = Hilo(lienzo.getLienzo(),5)
-        hilo1.start()
-        hilo2.start()
-        hilo3.start()
-        hilo4.start()
-        hilo5.start()
+        val hilo5 = Semaforo(lienzo.getLienzo())
+            hilo1.start()
+            hilo2.start()
+            hilo3.start()
+            hilo4.start()
+            hilo5.start()
+        //
     }
 }
